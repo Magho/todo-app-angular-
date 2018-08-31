f6b6e3cc285a81214b7b5e4aac36308f186e0dcc
Magho <mmaghraby134@gmail.com>
2018-08-31 18:20:20 +0200

    connect to firebsae

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyD9zBjYPZ1yJ906hVlUkj00Fpb5xWS0pSg",
+    authDomain: "todoapp-b0c36.firebaseapp.com",
+    databaseURL: "https://todoapp-b0c36.firebaseio.com",
+    projectId: "todoapp-b0c36",
+    storageBucket: "todoapp-b0c36.appspot.com",
+    messagingSenderId: "25058429486"
+  }
 };
 
 /*
