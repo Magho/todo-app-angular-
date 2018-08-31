f05a99d164fc9622446b521b9a7fa93050a7971e
Magho <mmaghraby134@gmail.com>
2018-08-31 19:44:28 +0200

    add delete & check functions to todocomponent

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -5,33 +5,40 @@ import { TodoService } from './shared/todo.service';
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
-  providers:[TodoService]
+  providers : [TodoService]
 })
 export class TodoComponent implements OnInit {
+  toDoListArray: any[];
+  constructor(private toDoService: TodoService) { }
 
-	toDoListArray : any[];
+  ngOnInit() {
+    this.toDoService.getToDoList().snapshotChanges()
+    .subscribe(item => {
+      this.toDoListArray = [];
+      item.forEach(element => {
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
 
-	constructor(private toDoService : TodoService) { }
+      //sort array isChecked false  -> true
+        this.toDoListArray.sort((a,b) => {
+          return a.isChecked - b.isChecked;
+        })
+    });
+  }
 
-  	ngOnInit() {
-  		this.toDoService.getToDoList().snapshotChanges()
-  		.subscribe(item => {
-  			this.toDoListArray = [];
-  			item.forEach(element => {
-  				var x = element.payload.toJSON();
-  				x["$key"] = element.key;
-  				this.toDoListArray.push(x);
-  			})
+  onAdd(itemTitle) {
+    this.toDoService.addTitle(itemTitle.value);
+    itemTitle.value = null;
+  }
 
-  			this.toDoListArray.sort((a,b) => {
-  				return a.isChecked - b.isChecked;
-  			})
-  		});
-  	}
+  alterCheck($key: string,isChecked) {
+    this.toDoService.checkorUnCheckTitle($key,!isChecked);
+  }
 
-    onAdd(itemTitle) {
-      this.toDoService.addTitle(itemTitle.value);
-      itemTitle.value = null;
-    }
+  onDelete($key : string){
+    this.toDoService.removeTitle($key);
+  }
 
 }
